1d3997bbed724f0787c8b32d322497faaefd9636
zhangwusan <zhangwusan1@gmail.com>
2024-02-15 19:19:08 +0700

    deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cafe3rd/",
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://meek-tail-production.up.railway.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: "/cafe3rd/"
 })
