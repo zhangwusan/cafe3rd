6dfe30fc20bb2a9604a7811a47b9f864e82051ee
zhangwusan <zhangwusan1@gmail.com>
2024-02-15 19:23:36 +0700

    deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cafe3rd/"
+  base: "/cafe3rd/",
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  }
 })
